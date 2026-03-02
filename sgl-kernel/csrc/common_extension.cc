1d0c7fc795c74a53ed20111505e1356001909949
rickojn@hotmail.com <rickojn@hotmail.com>
2026-03-02 00:05:14 +0000

    t4 removing refs from common_ext wip

--- sgl-kernel/csrc/common_extension.cc
+++ sgl-kernel/csrc/common_extension.cc
@@ -131,10 +131,10 @@ TORCH_LIBRARY_FRAGMENT(sgl_kernel, m) {
       "bias) -> Tensor");
   m.impl("int8_scaled_mm", torch::kCUDA, &int8_scaled_mm);
 
-  m.def(
-      "fp8_scaled_mm(Tensor mat_a, Tensor mat_b, Tensor scales_a, Tensor scales_b, ScalarType out_dtype, Tensor? "
-      "bias) -> Tensor");
-  m.impl("fp8_scaled_mm", torch::kCUDA, &fp8_scaled_mm);
+//   m.def(
+//       "fp8_scaled_mm(Tensor mat_a, Tensor mat_b, Tensor scales_a, Tensor scales_b, ScalarType out_dtype, Tensor? "
+//       "bias) -> Tensor");
+//   m.impl("fp8_scaled_mm", torch::kCUDA, &fp8_scaled_mm);
 
   m.def(
       "fp8_blockwise_scaled_mm(Tensor mat_a, Tensor mat_b, Tensor scales_a, Tensor scales_b, ScalarType out_dtype) -> "
