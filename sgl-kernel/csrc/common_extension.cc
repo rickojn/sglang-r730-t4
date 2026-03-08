f7bd30a0df8745ebdef9e09621d5c23f84306696
rickojn@hotmail.com <rickojn@hotmail.com>
2026-03-08 23:24:04 +0000

    trying to readd stuff

--- sgl-kernel/csrc/common_extension.cc
+++ sgl-kernel/csrc/common_extension.cc
@@ -154,8 +154,8 @@ TORCH_LIBRARY_FRAGMENT(sgl_kernel, m) {
 //   m.def("sgl_per_tensor_quant_fp8(Tensor input, Tensor! output_q, Tensor! output_s, bool is_static) -> ()");
 //   m.impl("sgl_per_tensor_quant_fp8", torch::kCUDA, &sgl_per_tensor_quant_fp8);
 
-//   m.def("sgl_per_token_quant_fp8(Tensor input, Tensor! output_q, Tensor! output_s) -> ()");
-//   m.impl("sgl_per_token_quant_fp8", torch::kCUDA, &sgl_per_token_quant_fp8);
+  m.def("sgl_per_token_quant_fp8(Tensor input, Tensor! output_q, Tensor! output_s) -> ()");
+  m.impl("sgl_per_token_quant_fp8", torch::kCUDA, &sgl_per_token_quant_fp8);
 
 //   m.def(
 //       "cutlass_scaled_fp4_mm(Tensor! out, Tensor a, Tensor b,"
